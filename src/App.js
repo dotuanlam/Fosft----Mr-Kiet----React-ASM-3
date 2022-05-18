02ee767caf7b4fa24452d00c285173dd84f1af07
nhatdaithienvuong <dotuanlam681@gmail.com>
2022-05-18 10:45:57 +0700

    nop react ASM 3 funtional component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputName, setInputName] = useState("");
+  const [infor, setInfor] = useState({
+    Role: "",
+    Company: "",
+    Email: "",
+    NumberOfFollowers: 0,
+    Avatar: null,
+  });
+
+  const getData = (name) => {
+    return fetch(`https://api.github.com/users/${name}`);
+  };
+
+  const onGettingData = () => {
+    getData(inputName)
+      .then((res) => res.json())
+      .then((res) => {
+        const avatarUser = res.avatar_url;
+        const role = res.type;
+        const company = res.company;
+        const email = res.email;
+        const followers = res.followers;
+        setInfor({
+          Role: role,
+          Company: company,
+          Email: email,
+          NumberOfFolowers: followers,
+          Avatar: avatarUser,
+        });
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="content">
+          <div className="form-input">
+            <input
+              onChange={(e) => setInputName(e.target.value)}
+              placeholder=" "
+            />
+            <label className="lable-input">Name</label>
+            <button className="btn-get" onClick={onGettingData}>
+              Get
+            </button>
+          </div>
+          <div className="UserDetailInfor">
+            <h1>User Detail Information</h1>
+          </div>
+          <div className="avatar">
+            <img alt="avatar" src={infor.Avatar}></img>
+          </div>
+          <h3>Role: {infor.Role === "null" ? "Updating" : infor.Role}</h3>
+          <h3>Email: {infor.Email === null ? "Updating" : infor.Email}</h3>
+          <h3>Number Of Follower: {infor.NumberOfFolowers}</h3>
+          <h3>
+            Company: {infor.Company === null ? "Updating" : infor.Company}
+          </h3>
+        </div>
+      </div>
     </div>
   );
 }
